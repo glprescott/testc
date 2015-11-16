8b2adbcbc078a187468a98949be9f3db62b56cf2
glprescott <glprescott@mailinator.com>
2015-11-16 13:54:37 +0100

    Trying out C

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main() {
+
+    printf("Hello world\n");
+
+    printf("Storage size for int : %d \n", sizeof(int));
+    printf("Storage size for float : %d \n", sizeof(float));
+    printf("Minimum float positive value: %E\n", FLT_MIN );
+    printf("Maximum float positive value: %E\n", FLT_MAX );
+    printf("Precision value: %d\n", FLT_DIG );
+
+    return 0;
+
+}
---
+++ hello.h
@@ -0,0 +1,8 @@
+//
+// Created by seb on 11/16/15.
+//
+
+#ifndef TESTC_HELLO_H
+#define TESTC_HELLO_H
+
+#endif //TESTC_HELLO_H
